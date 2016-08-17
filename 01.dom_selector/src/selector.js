5d78eddca0c851b60e1814e5f45b77fd67bf052d
itsderick <derickiscool@gmail.com>
2016-08-18 06:19:26 +0700

    Completing the first test involving jQuery-like DOM selector function.

--- 01.dom_selector/src/selector.js
+++ 01.dom_selector/src/selector.js
@@ -2,4 +2,32 @@ var domSelector = function(selectors) {
   'use strict';
 
   //code goes here
+  var regex = /^[0-9a-zA-Z]\w+$/;
+  var firstCharacter = selectors.charAt(0); // ANKI
+  var stringWithoutClassifier = selectors.substring(1); // ANKI
+  var elements;
+
+  if (firstCharacter === "#") {
+      elements = [document.getElementById(stringWithoutClassifier)];
+  } else if (firstCharacter === ".") {
+      elements = document.getElementsByClassName(stringWithoutClassifier);
+  } else {
+      if (selectors.match(regex) /* ANKI */ ) {
+          elements = document.getElementsByTagName(selectors);
+      } else {
+          throw new Error();
+      }
+  }
+
+  if ( ! elements || elements.length === 0) {
+      return [];
+  } else {
+      return [].slice.call(elements);
+  }
 };
+
+for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+        // Do shit here.
+    }
+}
