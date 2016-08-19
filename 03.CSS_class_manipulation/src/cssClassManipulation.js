f246a4a7c6cb8f634ab85975551e0f63035a3203
itsderick <derickiscool@gmail.com>
2016-08-19 08:31:30 +0700

    Completed an initial version of the class manipulation task for learn query.

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -2,4 +2,47 @@ var cssClass = (function() {
   'use strict';
 
   // code goes here
+
+  function add(element, classToAdd) {
+      var currentClassString = element.getAttribute('class'),
+          newClassString = "";
+
+      if ( ! currentClassString) {
+          element.setAttribute('class', classToAdd);
+      } else {
+          element.setAttribute('class', currentClassString + " " + classToAdd);
+      }
+  }
+
+  function remove(element, classToRemove) {
+      var currentClassString = element.getAttribute('class'),
+          newClassString = "";
+      if ( ! currentClassString || ! currentClassString.includes(classToRemove)) { /* ANKI */
+          return;
+      } else {
+          newClassString = currentClassString.split(classToRemove).join(" ").replace(/ +/g, " "); /* ANKI */
+          element.setAttribute('class', newClassString);
+      }
+  }
+
+  function has(element, classToCheck) {
+      var currentClassString = element.getAttribute('class');
+      return currentClassString ? currentClassString.includes(classToCheck) : false;
+  }
+
+  function toggle(element, classToToggle) {
+      var currentClassString = element.getAttribute('class');
+      if (has(element, classToToggle)) {
+          remove(element, classToToggle);
+      } else {
+          add(element, classToToggle);
+      }
+  }
+
+  return {
+      add: add,
+      remove: remove,
+      toggle: toggle,
+      has: has
+  };
 })();
